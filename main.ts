98e4a47db8be292d267e68784f35e3e06b4fa392
c.mirkin <ciromirkin@gmail.com>
2023-08-26 12:36:14 -0300

    add: typescript code for get the content of user name input

---
+++ main.ts
@@ -0,0 +1,26 @@
+
+const userNameForm = document.getElementById('userNameForm')!
+userNameForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const userName: string = getUserName();
+    if(IsValidUserName(userName)) {
+        userNameIsFine();
+        console.log(userName)
+    } else {
+        userNameIsWrong();
+    }
+})
+const getUserName = (): string => {
+    const userNameInput:HTMLInputElement = document.querySelector<HTMLInputElement>('#userNameInput')!;
+    return userNameInput.value.trim();
+}
+const IsValidUserName = (userName: string): boolean => !!userName;
+const userNameIsWrong = () => {
+    const userNameInput:HTMLInputElement = document.querySelector<HTMLInputElement>('#userNameInput')!;
+    userNameInput.classList.add('is-error');
+}
+const userNameIsFine = () => {
+    const userNameInput:HTMLInputElement = document.querySelector<HTMLInputElement>('#userNameInput')!;
+    userNameInput.classList.remove('is-error');
+    userNameInput.value = '';
+}
