bea14f3b9c57d03dc40dc1d186e5ee09801f658b
c.mirkin <ciromirkin@gmail.com>
2023-09-02 11:18:11 -0300

    add: two clases for get the information of a user by the name

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ userNameForm.addEventListener('submit', (e) => {
     const userName: string = getUserName();
     if(IsValidUserName(userName)) {
         userNameIsFine();
-        console.log(userName)
+        new SearchUserInfo().search(userName);
     } else {
         userNameIsWrong();
     }
@@ -23,4 +23,72 @@ const userNameIsFine = () => {
     const userNameInput:HTMLInputElement = document.querySelector<HTMLInputElement>('#userNameInput')!;
     userNameInput.classList.remove('is-error');
     userNameInput.value = '';
+}
+
+interface userInfoModel { 
+    photo: string,
+    bio: string,
+    userName: string,
+    name: string,
+    email: string,
+    date: string,
+    location: string,
+    site: string,
+    twiter: string
+}
+
+const showUserInfo = (userInfo: userInfoModel) => {
+    console.table(userInfo)
+}
+
+// 
+
+class SearchUserInfo {
+    constructor() {
+        
+    }
+
+    search(userName: string) {
+        const github = new Github()
+        const userdata = github.searchUser(userName, showUserInfo);
+    }
+}
+
+class Github {
+    URL_GitHub: string;
+    constructor() {
+        this.URL_GitHub = "https://api.github.com/users/";
+    }
+    searchUser(userName: string, showUserInfo: Function) {
+        const URL = `${this.URL_GitHub}${userName}`;
+        this.getUserInfo(URL, showUserInfo);
+    }
+    private getUserInfo(URL , callback: Function) {
+        fetch(URL).then( r => r.json()).then( userData => {
+            userData = this.formatUserData(userData);
+            callback(userData);
+        })
+    }
+    private formatUserData (userData): userInfoModel {
+        const formatDate = (date: string) => {
+            return new Date(date || "").toLocaleDateString("en-Us",{
+                year:"numeric",
+                month:"long",
+                day:"numeric",
+            })
+        }
+        const { avatar_url, bio, blog, created_at, email, location, name, login, twitter_username } = userData;
+        const userInfo: userInfoModel = {
+            photo: avatar_url,
+            userName: `@${login}`,
+            name,
+            bio,
+            date: formatDate(created_at || ""),
+            location: (location || ""),
+            email: (email || ""),
+            site: (blog || ""),
+            twiter: (twitter_username || ""),
+        }
+        return userInfo
+    }
 }
